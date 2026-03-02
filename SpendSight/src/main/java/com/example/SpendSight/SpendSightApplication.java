a3b1ff1645751141fd5d7a8244c0a3a1173d1840
Andres Susrez <andresfelipepintosuarez14@gmail.com>
2026-03-02 10:29:08 -0500

    actualizando datos

---
+++ SpendSight/src/main/java/com/example/SpendSight/SpendSightApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpendSight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendSightApplication.class, args);
+	}
+
+}
